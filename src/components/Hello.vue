b0e8fc7d1661abed0f4a8cf7075dbe625b88ad39
Acparvis <acparvis@googlemail.com>
2017-09-30 17:50:09 +0100

    Made some changes

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>{{ number }}</h1>
+    <button @click="doMath"> Press me! </button>
+    <p>{{ watchMath }}</p>
+    <Shadow :num="number"
+    ></Shadow>
   </div>
 </template>
 
 <script>
+import Shadow from './shadow.vue'
 export default {
   name: 'hello',
+  components: {
+    Shadow
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      number: 2
+    }
+  },
+  methods: {
+    doMath: function () {
+      this.number += 2
+    }
+  },
+  computed: {
+    watchMath: function () {
+      if (this.number > 10) {
+        return 'Greater than 10!'
+      }
     }
   }
 }
---
+++ src/components/shadow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="shadow">
+		<h1>Shadow {{ num }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'shadow',
+    data () {
+      return {
+        msg: ''
+      }
+    },
+    props: ['num']
+  }
+</script>
+
+<style scoped>
+
+</style>
